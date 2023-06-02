eca81e19b0f2ec5b8eb9730b7258baddd96241cd
elgrako <103579462+elgrako@users.noreply.github.com>
2023-06-02 19:15:15 +0200

    Add files via upload

---
+++ SubscripcionTest2.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SubscripcionTest2 {
+
+	@Test
+	void testSubscripcion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPrecioPorMes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCancel() {
+		fail("Not yet implemented");
+	}
+
+}
